5455f919e3ea4398de7772be05fc58ad96514c78
Mixer27 <mikikorolczyk@gmail.com>
2024-02-28 19:57:40 +0000

    clunky routing

---
+++ client/src/home/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import '../index.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <h1>Home</h1>
+    <a href="../../about">about</a>
+  </React.StrictMode>,
+)
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,18 +4,19 @@ import fs from 'fs'
 import path from 'path'
 import 'dotenv/config'
 
-const { HOST, PORT , SSL_KEY, SSL_CERT } = process.env;
+const { HOST, PORT, SSL_KEY, SSL_CERT } = process.env;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // root: "src",
   server: {
     host: `${HOST}`,
     port: parseInt(PORT),
     https: {
       key: fs.readFileSync(path.resolve(__dirname, `${SSL_KEY}`)),
       cert: fs.readFileSync(path.resolve(__dirname, `${SSL_CERT}`))
-    }
+    },
 
   },
   preview: {
@@ -25,5 +26,16 @@ export default defineConfig({
       key: fs.readFileSync(path.resolve(__dirname, `${SSL_KEY}`)),
       cert: fs.readFileSync(path.resolve(__dirname, `${SSL_CERT}`))
     }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "index.html"),
+        home: path.resolve(__dirname, "home.html"),
+        about: path.resolve(__dirname, "about.html"),
+        // about: path.resolve(__dirname, 'src/about/index.html'),
+      },
+    }
   }
+
 })
